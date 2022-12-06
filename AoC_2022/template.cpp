51b7e767258be69594a27b71d7d9c5ae3a1bb7b0
Gaston Sandstig <sandstig@student.chalmers.se>
2022-12-06 07:34:36 +0100

    added template cpp file

---
+++ AoC_2022/template.cpp
@@ -0,0 +1,20 @@
+#include "../myTools.h"
+
+const char defaultFilePath[] = "AoC_20XX/YY/input.txt";
+
+void parse(const char f[]){
+
+    ifstream inputStream(f);
+    if( !inputStream.is_open() )
+        abort();
+
+    inputStream.close();
+    return;
+}
+
+int main(){
+
+    parse(defaultFilePath);
+
+    return 0;
+}
